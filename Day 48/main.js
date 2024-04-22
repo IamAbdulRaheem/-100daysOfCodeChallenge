a1328e25f455359538430490a0071aa224d0afb2
Abdul Raheem <160466726+IamAbdulRaheem@users.noreply.github.com>
2024-04-22 12:46:55 -0700

    #100daysOfCodeChallenge

---
+++ Day 48/main.js
@@ -0,0 +1,30 @@
+"use strict";
+// Task 142: Creating a promise
+Object.defineProperty(exports, "__esModule", { value: true });
+const helloPromise = new Promise((resolve) => {
+    setTimeout(() => {
+        resolve("Hello World!");
+    }, 2000);
+});
+helloPromise.then((message) => console.log(message));
+// Task 143: .then() and .catch()
+const conditionalPromise = new Promise((resolve, reject) => {
+    const success = Math.random() > 0.5;
+    if (success) {
+        resolve("Success!");
+    }
+    else {
+        reject(new Error("Failure!"));
+    }
+});
+conditionalPromise.then((result) => console.log(result)); // Handles successfull resolution
+conditionalPromise.catch((error) => console.log(error.message)); // Handles rejection
+// Task 143: .promiseAll()
+const promise1 = Promise.resolve(3);
+const promise2 = 42;
+const promise3 = new Promise((resolve) => {
+    setTimeout(resolve, 100, "foo");
+});
+Promise.all([promise1, promise2, promise3]).then((values) => {
+    console.log(values);
+});
